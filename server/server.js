95c0e297478f268ee55fd05bc6c097a50d5e2f39
Denis Son <sondenis90@gmail.com>
2022-04-19 20:48:00 +0200

    env

--- server/server.js
+++ server/server.js
@@ -4,18 +4,20 @@ const expressGraphQL = require("express-graphql");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const schema = require("./schema/schema");
+require("dotenv").config();
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI =
-  "mongodb+srv://denis-admin:iSsb9oKkyjXxJ4EV@cluster0.bccxe.mongodb.net/test";
+
+const MONGO_URI = process.env.MONGODB_URI;
+
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoLab URI");
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection
   .once("open", () => console.log("Connected to MongoLab instance."))
   .on("error", (error) => console.log("Error connecting to MongoLab:", error));
